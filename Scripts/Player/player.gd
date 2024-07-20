1029660cb75fbdf1c68ff2b6cd7690abc131c5c2
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-07-19 23:01:59 -0400

    Hurt Flash

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,12 +3,19 @@ class_name Player
 @export var normal_speed = 100.0
 @export var acceleration = 100.0
 @onready var health = $Components/Health
+@onready var flash = $Components/Flash
+@onready var hurtbox_light = $Areas/HurtboxLight
+@onready var hurtbox_shadow = $Areas/HurtboxShadow
 
 var max_speed
 
 func _ready():
 	max_speed = normal_speed
+	hurtbox_light.hurt.connect(func(hitbox: Hitbox):
+		flash.flash()
+		)
 	health.no_health.connect(die)
+
 
 func die():
 	queue_free()
