9188fa0d443f4d315f1224c4fdb5fe971c2acb9e
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-07-19 21:27:25 -0400

    HP, Hitbox and Hurtbox

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,7 +2,13 @@ extends CharacterBody2D
 class_name Player
 @export var normal_speed = 100.0
 @export var acceleration = 100.0
+@onready var health = $Components/Health
+
 var max_speed
 
 func _ready():
 	max_speed = normal_speed
+	health.no_health.connect(die)
+
+func die():
+	queue_free()
