54ad54b4e3d94a7c7a752f698b5c55e9e81e34bf
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-07-27 12:17:55 -0400

    UI Fix

--- Scripts/UI/cooldownbar.gd
+++ Scripts/UI/cooldownbar.gd
@@ -2,16 +2,21 @@ extends Control
 @export var progressBar: TextureProgressBar
 @export var progressTimer: Timer
 @export var updateTimer: Timer
+@export var label: Label
 var fillTime = 0
-
+var dashInput
 
 func _ready():
+	update_label()
 	progressBar.value = 0
 	progressTimer.wait_time = 0.05
 	updateTimer.wait_time = 0.05
 	Signals.dashCooldownBar.connect(cooldown_value)
 	Signals.dashBar.connect(update_value)
 
+func update_label():
+	dashInput = InputMap.action_get_events("dash")[0]
+	label.text = "Dash: " + dashInput.as_text().trim_suffix(" (Physical)")
 
 func update_value(time):
 	show()
